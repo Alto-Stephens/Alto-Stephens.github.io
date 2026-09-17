35fd63d724ae6d3fc80cc4a8f3de6f65cc8d2efb
Alto Stephens <eviesbel@gmail.com>
2026-09-17 15:25:19 -0400

    added midnight arcade homepage

--- csce242/exercises/04Jjava/scripts/intro.js
+++ csce242/exercises/04Jjava/scripts/intro.js
@@ -19,3 +19,90 @@ document.getElementById("link").onclick = (e) =>{
 document.getElementById("animation-btn").onclick =  () => {
     document.getElementById("ball").classList.toggle("bouncing-ball")
 }
+
+/*Plant health*/
+document.getElementById("num-days").onkeyup = (e)=>{
+    const numDays=parseInt(e.target.value);
+    const pMessage = document.getElementById("plant-msg");
+    pMessage.innerHTML = numDays;
+    const plantImage = document.getElementById("img-plant")
+
+    if(numDays >= 0 && numDays <=2){
+        if(numDays == 1){
+            pMessage.innerHTML = `Let your plant rest it's only been ${numDays} day`;
+            plantImage.src="https://static.vecteezy.com/system/resources/thumbnails/057/377/760/small/concept-of-circular-economy-with-eco-friendly-product-lifecycle-recyclable-materials-in-focus-photo.jpg"; 
+        }else{
+            pMessage.innerHTML = `Let your plant rest it's only been ${numDays} days`;
+        }
+    }else if(numDays <= 5){
+            pMessage.innerHTML = `Time to water it's been ${numDays} days!`;
+
+    }else if(numDays <= 7){
+            pMessage.innerHTML = `Your plant is dying it's been ${numDays} days since watering`;
+
+    }else{
+            pMessage.innerHTML =   `Your plant is dead it's been ${numDays} days since watering`;
+    }
+}
+
+/*Counter*/
+let countInterval;
+let count = 0;
+const pCount = document.getElementById("p-count");
+const btnStart=document.getElementById("btn-start");
+const btnPause = document.getElementById("btn-pause");
+const btnStop = document.getElementById("btn-stop");
+
+btnPause.disabled = true;
+btnStop.disabled = true;
+
+
+
+document.getElementById("btn-start").onclick = () =>{
+    countInterval = setInterval(()=>{
+        pCount.innerHTML = ++count;
+    },1000);
+    btnStart.disabled = true;
+    btnPause.disabled = false;
+    btnStop.disabled = false;
+}
+document.getElementById("btn-pause").onclick = () =>{
+    clearInterval(countInterval);
+    btnStart.disabled = false;
+    btnPause.disabled = true;
+    btnStop.disabled = false;
+
+}
+document.getElementById("btn-stop").onclick = () =>{
+    count=0;
+    pCount.innerHTML="";
+    btnStart.disbaled = false;
+    btnPause.disabled = false;
+    btnStop.disabled = true;
+}
+
+/*Date Display*/
+setInterval(()=>{
+    const pDisplay = document.getElementById("date-display");
+    const today = new Date();
+    const secs = today.getSeconds();
+    const mins = today.getMinutes();
+    const hours = today.getHours();
+
+    const day = today.getDate();
+    const month = today.getMonth() + 1;
+    const year = today.getFullYear();
+
+    pDisplay.innerHTML = `${hours}:${mins}:${secs}  ${month}/${day}/${year}`
+
+
+},1000);
+
+/*Thermometer*/
+document.getElementById("btn-donation").onclick =()=>{
+    const userDonation = parseInt(document.getElementById("txt-donation").value);
+    const donationP = document.getElementById("donation-txt");
+    const percent = (userDonation /  10000)*100;
+
+    donationP.innerHTML = `You are ${percent.toFixed(1)}% to your goal`;
+}
